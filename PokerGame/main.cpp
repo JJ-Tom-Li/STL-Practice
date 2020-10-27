63678845249b764916f1f9bd4e7d612a4cd22903
Jia-Jun Li <tom.lee@wanin.tw>
2020-10-27 16:46:37 +0800

    Initial Commit

---
+++ PokerGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PokerGame
+//
+//  Created by user on 2020/10/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
