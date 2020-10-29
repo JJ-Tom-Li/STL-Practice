9d73ae4c40d41f7f9b5a2a298313945f08420701
Jia-Jun Li <tom.lee@wanin.tw>
2020-10-29 17:37:21 +0800

    把card.suit改成std::string型態，並修正相關method，將花色改成中文顯示，並新增1,11,12,13在ToString會變成A,J,Q,K

--- PokerGame/Card.h
+++ PokerGame/Card.h
@@ -12,35 +12,35 @@
 #define NUMBER_OF_CARD_SUITS 4
 #define MAX_CARD_NUMBER 13
 #define NUMBER_OF_POKER_CARDS 52
+static const std::string suits[NUMBER_OF_CARD_SUITS] = {"黑桃", "紅心", "方塊", "梅花"};
 class Card {
 public:
     Card();
-    Card(char, int);
+    Card(std::string, int);
     static std::vector<Card> GeneratePokerCards();
     
     bool Equals(Card);
     bool SuitEquals(Card);
-    bool SuitEquals(char);
+    bool SuitEquals(std::string);
     bool NumberEquals(Card);
     bool NumberEquals(int);
     int Sub(Card);
     std::string ToString();
     
-    static Card StringToCard(std::string);
     static std::vector<Card> Sort(std::vector<Card>);
 private:
-    constexpr static const char suits[NUMBER_OF_CARD_SUITS] = {'S', 'H', 'D', 'C'};
-    char suit;
+    
+    std::string suit;
     int number;
     static bool CardCompare(Card, Card);
 };
 
 Card::Card(){
-    this->suit = '0';
+    this->suit = " ";
     this->number = 0;
 }
 
-Card::Card(char suit, int number){
+Card::Card(std::string suit, int number){
     this->suit = suit;
     this->number = number;
 }
@@ -53,7 +53,7 @@ bool Card::SuitEquals(Card card){
     return (this->suit == card.suit);
 }
 
-bool Card::SuitEquals(char suit){
+bool Card::SuitEquals(std::string suit){
     return (this->suit == suit);
 }
 
@@ -74,7 +74,7 @@ std::vector<Card> Card::GeneratePokerCards(){
     cards.assign(NUMBER_OF_POKER_CARDS, Card());
     for (int i = 0; i < NUMBER_OF_CARD_SUITS; i++) {
         for (int j = 0; j < MAX_CARD_NUMBER; j++) {
-            cards[i * MAX_CARD_NUMBER + j] = Card(Card::suits[i], j + 1);
+            cards[i * MAX_CARD_NUMBER + j] = Card(suits[i], j + 1);
         }
     }
     
@@ -82,17 +82,30 @@ std::vector<Card> Card::GeneratePokerCards(){
 }
 
 std::string Card::ToString(){
+    std::string suitString;
+    std::string numberString;
     std::string cardToString;
-    cardToString = this->suit + std::to_string(this->number);
+    suitString = this->suit;
+    
+    switch (this->number) {
+        case 1:
+            numberString = "A";
+            break;
+        case 11:
+            numberString = "J";
+            break;
+        case 12:
+            numberString = "Q";
+            break;
+        case 13:
+            numberString = "K";
+            break;
+        default:
+            numberString = std::to_string(this->number);
+            break;
+    }
+    cardToString = suitString + numberString;
     return cardToString;
-}
-
-Card Card::StringToCard(std::string cardString){
-    Card card;
-    card.suit = cardString[0];
-    card.number = std::stoi(cardString.substr(1));
-    //std::cout << card.suit << "," << card.number << std::endl;
-    return card;
 }
 
 std::vector<Card> Card::Sort(std::vector<Card> cards){
